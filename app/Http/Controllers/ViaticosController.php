f226f1f418d680796495f7311e01338e1c7f0f41
JosueArturo <69615043+JosueArturo@users.noreply.github.com>
2021-03-15 19:57:40 -0600

    Crud Viaticos

--- app/Http/Controllers/ViaticosController.php
+++ app/Http/Controllers/ViaticosController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\t_viaticos;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class ViaticosController extends Controller
 {
@@ -13,7 +16,10 @@ class ViaticosController extends Controller
      */
     public function index()
     {
-        return view('Viaticos');
+        date_default_timezone_set('America/Costa_Rica');
+        $date = Carbon::now()->locale('es_ES');
+        $Viaticos = DB::table('t_viaticos')->get();
+        return view('Viaticos' , ['t_viaticos' => $Viaticos]);
 
     }
 
@@ -35,7 +41,16 @@ class ViaticosController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $agregar = new t_viaticos();
+        $agregar->tipo_de_vehiculo = $request->tipo_de_vehiculo;
+        $agregar->antiguedad_vehiculo_años = $request->antiguedad_vehiculo_años;
+        $agregar->tarifa_km_recorrido = $request->tarifa_km_recorrido;
+        $agregar->km_recorridos = $request->km_recorridos;
+        $agregar->total_km = $agregar->tarifa_km_recorrido*$agregar->km_recorridos;
+        // Insertar en la base de datos
+        $agregar->save();
+        // Redirigir a la vista original 
+         return back()->with('agregar', 'Viatico se ha agregado');
     }
 
     /**
--- app/t_viaticos.php
+++ app/t_viaticos.php
@@ -9,4 +9,5 @@ class t_viaticos extends Model
     protected $primaryKey = "id_viatico";
     protected $remember_token = false;
     protected $table = "t_viaticos";
+    public $timestamps = false;
 }
