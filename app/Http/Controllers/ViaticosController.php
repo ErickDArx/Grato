4a02e674e92127211022022effd4b6c2faa463d2
ErickDArx <alexalonso2706@gmail.com>
2021-03-15 20:14:31 -0600

    new

--- app/Http/Controllers/ViaticosController.php
+++ app/Http/Controllers/ViaticosController.php
@@ -53,12 +53,6 @@ class ViaticosController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
         //
