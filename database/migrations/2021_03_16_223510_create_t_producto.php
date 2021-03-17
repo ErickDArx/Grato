4cf5e7dd4aa53ea0e8daf942ca64b913140ced72
JosueArturo <69615043+JosueArturo@users.noreply.github.com>
2021-03-16 18:37:29 -0600

    perro

---
+++ database/migrations/2021_03_16_223510_create_t_producto.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTProducto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_producto', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->bigIncrements('id_producto');
+            $table->string('nombre_producto');
+        });
+    }
+
+    /**hola
+     * 
+     * 
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_producto');
+    }
+}
